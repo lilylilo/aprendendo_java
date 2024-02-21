d0bb375c45433617ba1dcb1a76c3b6cb0601b7e7
Lily <133771406+lilylilo@users.noreply.github.com>
2024-02-21 16:38:24 -0300

    Caderno de Exercício (Lista 1)

---
+++ Ex01.java
@@ -0,0 +1,9 @@
+/* Lista 1- exercício 1
+   Crie um programa que recebe três nomes quaisquer por meio da linha de execução do programa (prompt), e os imprima na tela da seguinte maneira: o primeiro e o último nome serão impressos na primeira linha um após o outro, o outro nome (segundo) será impresso na segunda linha. */
+
+public class Ex01{
+	public static void main(String args[]){
+		System.out.println(args[0]+" "+args[2]);
+		System.out.println(args[1]);
+	}
+}
