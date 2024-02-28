a26feaedc9953b724d435757fd9e1cb11dd43bca
Lily <133771406+lilylilo@users.noreply.github.com>
2024-02-28 10:54:13 -0300

    Add files via upload

---
+++ App5.java
@@ -0,0 +1,21 @@
+package aula2802;
+
+public class App5 {
+
+	public static void main(String[] args) {
+		
+		String palavra = "Caraguatatuba";
+		System.out.println("Nº caracteres: "+palavra.length());
+		System.out.println(palavra.toLowerCase());
+		System.out.println(palavra.toUpperCase());
+		System.out.println(palavra.charAt(4));
+		System.out.println(palavra.indexOf("g"));
+		System.out.println(palavra.indexOf("ta"));
+		System.out.println(palavra.substring(3,7));
+		
+		int a = 10;
+		System.out.println(a);
+		
+	}
+
+}
