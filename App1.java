7e3d745e939c9c586a9aff848bc9f3a757d7a154
Lily <133771406+lilylilo@users.noreply.github.com>
2024-02-28 10:53:20 -0300

    Add files via upload

--- App1.java
+++ App1.java
@@ -1,6 +1,10 @@
-public class App1{
-	public static void main(String args[]){
-		System.out.println("Nome 1: "+args[0]);
-		System.out.println("Nome 2: "+args[1]);
+        package aula2802;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		System.out.println("Alo Mundo");
+
 	}
+
 }
