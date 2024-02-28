b3d0be3c3b7cdb31b5a7986caabcf81a3583c20d
Lily <133771406+lilylilo@users.noreply.github.com>
2024-02-28 10:53:48 -0300

    Add files via upload

---
+++ App3.java
@@ -0,0 +1,14 @@
+package aula2802;
+
+public class App3 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println( Math.abs(-10)); // Resultado igual a 10
+		System.out.println( Math.sqrt(25)); // Resultado igual a 25
+		System.out.println( Math.pow(2,3)); // Resultado igual a 8
+		System.out.println( (int) (Math.random()*100) ); // Resultado: qualquer valor entre 0.0 (inclusive) e menor que 1.0 
+
+	}
+	
+}
