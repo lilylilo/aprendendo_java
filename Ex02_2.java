f9ddc39ff31cd10aa2820713893530e2e7b7c62d
Lily <133771406+lilylilo@users.noreply.github.com>
2024-03-02 10:44:10 -0300

    Lista de Exercícios 2

---
+++ Ex02_2.java
@@ -0,0 +1,12 @@
+package aula2802;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		
+		int soma = 0;
+		for(int i=1; i<=100; i++) {
+			soma = soma +i;
+		}
+		System.out.println("Soma= "+soma);
+	}
+}
