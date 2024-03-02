feea221694f77d0469f8887107639e0d0accdf53
Lily <133771406+lilylilo@users.noreply.github.com>
2024-03-02 10:34:49 -0300

    Create Ex01_2.java

---
+++ Ex01_2.java
@@ -0,0 +1,29 @@
+package aula2802;
+// Lista 2
+
+import java.util.Scanner;
+
+public class Ex01 { 
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite valor de A: ");
+		int a = sc.nextInt();
+		
+		System.out.println("Digite valor de B: ");
+		int b = sc.nextInt();
+		
+		System.out.println("Digite valor de C: ");
+		int c = sc.nextInt();
+		
+		if(a>=b && a>=c)
+			System.out.println("A é maior "+a);
+		else if(b>=c)
+			System.out.println("B é maior "+b);
+		else
+			System.out.println("C é maior "+c);
+	}
+
+}
