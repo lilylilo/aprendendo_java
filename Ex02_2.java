a2ed3f02c3b27405d9eb302183d11f898d9e1f8d
Lily <133771406+lilylilo@users.noreply.github.com>
2024-03-02 10:48:31 -0300

    Ex02_2.java

---
+++ Ex02_2.java
@@ -0,0 +1,12 @@
+package aula2802;
+//Faça um aplicativo que calcule o produto dos inteiros ímpares de 1 a 15 eexiba o resultado na tela
+
+public class Ex02 {
+	public static void main(String[] args) {
+		int produto = 1;
+		for(int i=1; i<=15; i+=2) {
+			produto = produto *i;
+		}
+		System.out.println("Produto= "+produto);
+	}
+}
